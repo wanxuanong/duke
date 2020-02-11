92253baa88acd923657dba02b9f861559d14b916
WanXuanOng <wanxuanong1@gmail.com>
2020-02-11 23:49:41 +0800

    A-Assertions

--- src/main/java/Tasklist.java
+++ src/main/java/Tasklist.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 
-public class Tasklist {
-    public Tasklist(ArrayList<Task> list) {
+public class TaskList {
+    protected ArrayList<Task> list;
+
+    public TaskList(ArrayList<Task> list) {
         this.list = list;
+    }
+
+    public Task getTask(int idx) {
+        return list.get(idx);
     }
 
     public int getListSize() {
         return list.size();
     }
+
+    public
 }
 
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class Ui {
-    public String intro() {
-        return "Hello friend. What can I do for you? xD";
+    public Ui() {
+
     }
 
+    public String input() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
 
+    public void intro() {
+        String intro = "Hello friend. What can I do for you? xD";
+        System.out.println(intro);
+    }
+
+    public void exit() {
+        String exit = "Bye!!! See ya again soon xD";
+        System.out.println(exit);
+    }
+
+    public void printList(TaskList taskList) {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskList.getListSize(); i++) {
+            System.out.println((i + 1) + "." + taskList.getTask(i).toString());
+        }
+    }
+
+    public void printDoneTask(Task task) {
+        System.out.println("Nice! I've marked this as done:" + "\n  " + task.toString());
+    }
+
+    public void printDeletedTask(Task task, TaskList taskList) {
+        System.out.println("Noted. I've removed this task:\n  " + task.toString() + "\n" +
+                "Now you have " + taskList.getListSize() + " tasks in the list."););
+    }
+
+    public void printAddedTask(Task task, TaskList taskList) {
+        System.out.println("Got it. I've added this task:\n  " + task.toString() + "\n" +
+                "Now you have " + taskList.getListSize() + " tasks in the list.");
+    }
 }
 
