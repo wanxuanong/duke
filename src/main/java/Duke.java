2facacd56372be4233ac633decea0e9bed8b40fe
WanXuanOng <wanxuanong1@gmail.com>
2020-02-12 13:52:40 +0800

    JavaFxTutorialPart1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Duke {
+public class Duke extends Application {
     private TaskList taskList;
     private Storage storage;
     private Ui ui;
-    static String path = "/Users/ongwanxuan/duke/src/main/java/tasks.txt";
+    static String path = "data/tasks.txt";
+
+    public Duke() {}
 
     public Duke(String path) throws IOException {
         this.ui = new Ui();
@@ -38,141 +44,14 @@ public class Duke {
     public static void main(String[] args) throws IOException, DukeException {
         new Duke(path).run();
     }
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello World!"); // Creating a new Label control
+        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+
+        stage.setScene(scene); // Setting the stage to show our screen
+        stage.show(); // Render the stage.
+    }
+
 }
-
-    /**static ArrayList<Task> list = new ArrayList<Task>(100);
-
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello friend. What can I do for you? xD\n");
-        Scanner sc = new Scanner(System.in);
-        String path = "/Users/ongwanxuan/duke/src/main/java/tasks.txt";
-        File file = new File("/Users/ongwanxuan/duke/src/main/java/tasks.txt");
-        boolean exists = file.exists();
-
-        if (!exists) {
-            PrintWriter pw = new PrintWriter("tasks.txt", "UTF-8");
-            pw.println("");
-        } else {
-            try {
-                fileContents(path, list);
-            } catch (IOException e) {
-                System.out.println("Load failed.");
-            }
-        }
-        while (sc.hasNextLine()) {
-            try {
-                String command = sc.nextLine();
-                String[] commandArr = command.split(" ", 2);
-
-                if (command.equals("bye")) {
-                    System.out.println("Bye!!! See ya again soon xD");
-                    break;
-                } else if (command.equals("list")) {
-                    System.out.println("Here are the tasks in your list:");
-                    for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + "." + list.get(i).toString());
-                    }
-                } else if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
-                    throw new DukeException("☹ OOPS!!! The description of a " + command + " cannot be empty.");
-
-                } else {
-                    Task t = new Task(command);
-                    if ((commandArr[0]).equals("done")) {
-                        int i = Integer.parseInt(commandArr[1]);
-                        list.get(i - 1).markDone();
-                        System.out.println("Nice! I've marked this as done:" + "\n  " + list.get(i - 1).toString());
-                        writeNew(path, list);
-
-                    } else if ((commandArr[0]).equals("delete")) {
-                        int i = Integer.parseInt(commandArr[1]);
-                        Task d = list.get(i - 1);
-                        list.remove(d);
-                        System.out.println("Noted. I've removed this task:\n  " + d.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.");
-                        writeNew(path, list);
-
-                    } else if ((commandArr[0]).equals("todo")) {
-                        Task t1 = new Todo(commandArr[1]);
-                        list.add(t1);
-                        System.out.println("Got it. I've added this task:\n  " + t1.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.");
-                        writeToOld(path, t1.formatText());
-
-                    } else if ((commandArr[0].equals("deadline"))) {
-                        String[] byArr = commandArr[1].split("/by ");
-                        Task t2 = new Deadline(byArr[0], byArr[1]);
-                        list.add(t2);
-                        System.out.println("Got it. I've added this task:\n" + "  " + t2.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.");
-                        writeToOld(path, t2.formatText());
-
-                    } else if ((commandArr[0].equals("event"))) {
-                        String[] atArr = commandArr[1].split("/at ");
-                        Task t3 = new Event(atArr[0], atArr[1]);
-                        list.add(t3);
-                        System.out.println("Got it. I've added this task:\n" + "  " + t3.toString() + "\n" +
-                                "Now you have " + list.size() + " tasks in the list.");
-                        writeToOld(path, t3.formatText());
-                    } else {
-                        throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    }
-                }
-            } catch (DukeException m) {
-                System.out.println(m.getMessage());
-            } catch (IOException e) {
-                System.out.println("File not found...");
-            }
-        }
-
-    }
-    public static void fileContents(String path, ArrayList<Task> list) throws IOException {
-        File f = new File(path); //create a file for the given file path
-        Scanner sc = new Scanner(f);
-        while (sc.hasNext()) {
-            String s = sc.nextLine();
-            String[] arr = s.split(" [|] ");
-            String type = arr[0];
-            switch(type) {
-                case "T":
-                    Todo t1 = new Todo(arr[2]);
-                    if (arr[1].equals("1")) {
-                        t1.markDone();
-                    }
-                    list.add(t1);
-                    break;
-
-                case "D":
-                    Deadline d1 = new Deadline(arr[2], arr[3]);
-                    if (arr[1].equals("1")) {
-                        d1.markDone();
-                    }
-                    list.add(d1);
-                    break;
-
-                case "E":
-                    Event e1 = new Event(arr[2], arr[3]);
-                    if (arr[1].equals("1")) {
-                        e1.markDone();
-                    }
-                    list.add(e1);
-                    break;
-
-                default:
-                    break;
-            }
-        }
-    }
-
-    public static void writeNew(String path, ArrayList<Task> list) throws IOException {
-        FileWriter fw = new FileWriter(path);
-        for (Task task : list) {
-            fw.write(task.formatText());
-        }
-        fw.close();
-    }
-
-    public static void writeToOld(String path, String newText) throws IOException {
-        FileWriter fw = new FileWriter(path, true);
-        fw.write(newText);
-        fw.close();
-    }**/
