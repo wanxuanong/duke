ff5d6b3b51b6542381fc77b91138687b9b9dbdeb
WanXuanOng <wanxuanong1@gmail.com>
2020-02-18 23:12:48 +0800

    Removed unused files.

--- src/test/java/TodoTest.java
+++ src/test/java/TodoTest.java
@@ -1,7 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TodoTest {
     @Test
-
-
-
+    public void todoTest() {
+        Task task = new Todo("test");
+        String output = "[T][✗] test";
+        assertEquals(output, task.toString());
+        String output1 = "T | 0 | test\n";
+        assertEquals(output1, task.formatText());
+    }
 }
 
